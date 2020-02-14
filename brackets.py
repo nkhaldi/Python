03bef573f014627c4e326f529593d49d7a2a6329
Narek <mnk80898@gmail.com>
2020-02-14 17:36:29 +0300

    added brackets

---
+++ brackets.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#	Программа проверяющая правильность расстановки скобок в строке
+
+def brackets(brac):
+    stack = []
+    for el in brac:
+        if el in ['[', '(', '{', '<']:
+            stack.append(el)
+        elif el in [']', ')', '}', '>']:
+            if len(stack) > 0:
+                top = stack.pop()
+                if top + el not in ['[]', '()', '{}', '<>']:
+                    return False
+            else:
+                return False
+
+    return len(stack) == 0
+
+if brackets(input()):
+	print("Success")
+else:
+	print ("Fail")
