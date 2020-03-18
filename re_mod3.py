6817dd0497365c5e0a9eb706342604cce4ee42b8
Narek <mnk80898@gmail.com>
2020-03-19 00:39:06 +0300

    added re_mod3.py

---
+++ re_mod3.py
@@ -0,0 +1,15 @@
+# Вам дана последовательность строк.
+# Выведите строки, содержащие двоичную запись числа, кратного 3.
+# Решить задачу, используя регулярные выражения.
+
+import re
+import sys
+
+
+pattern = "^(0|(1(01*0)*1))*$"
+pattern = re.compile(pattern)
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if pattern.match(line):
+        print(line)
