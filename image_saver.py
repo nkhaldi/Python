60d5d2a64e3dd8bab2399033ce050863dc7ea759
Narek <mnk80898@gmail.com>
2020-03-19 01:22:24 +0300

    added image_saver & url_ref

---
+++ image_saver.py
@@ -0,0 +1,8 @@
+# Скрипт для сохранения изображений по url
+
+import requests
+
+img_url = input()
+res = requests.get(img_url)
+with open("img.png", "wb") as f:
+    f.write(res.content)
---
+++ url_ref.py
@@ -0,0 +1,33 @@
+# Рассмотрим два HTML-документа A и B.
+# Из A можно перейти в B за один переход, если в A
+# есть ссылка на B, т. е. внутри A есть тег <a href="B">,
+# возможно с дополнительными параметрами внутри тега.
+# Из A можно перейти в B за два перехода если существует
+# такой документ C, что из A в C можно перейти за один переход
+# и из C в B можно перейти за один переход.
+
+# Вашей программе на вход подаются две строки,
+# содержащие url двух документов A и B.
+# Выведите Yes, если из A в B можно перейти за два перехода,
+# иначе выведите No.
+
+# Обратите внимание на то, что не все ссылки внутри
+# HTML документа могут вести на существующие HTML документы.
+
+import re
+import requests
+
+
+def find_ref(link1, link2):
+    link_pattern = re.compile(r'<a[^>]*?href="(.*?)"[^>]*?>')
+    resp = requests.get(link1).text
+    for url in link_pattern.findall(resp):
+        cur_resp = requests.get(url).text
+        if link2 in link_pattern.findall(cur_resp):
+            return True
+    return False
+
+
+link1 = input()
+link2 = input()
+print("Yes" if find_ref(link1, link2) else "No")
