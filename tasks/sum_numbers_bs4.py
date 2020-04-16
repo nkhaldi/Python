f692220053fe5c26dd2e544ad2c3f7cf09185609
Narek <mnk80898@gmail.com>
2020-04-16 21:58:53 +0300

    used diffucult html-page to parse

--- tasks/sum_numbers_bs4.py
+++ tasks/sum_numbers_bs4.py
@@ -8,7 +8,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-url = "https://stepik.org/media/attachments/lesson/209723/3.html"
+url = "https://stepik.org/media/attachments/lesson/209723/5.html"
 html = str(urlopen(url).read().decode('utf-8'))
 soup = BeautifulSoup(html, 'html.parser')
 
