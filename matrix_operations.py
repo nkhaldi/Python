b39b92a4a396b37a25e39d07a83d630929a7f578
nkhaldi <mnk80898@gmail.com>
2021-10-15 15:42:52 +0300

    added matrix sum

--- matrix_operations.py
+++ matrix_operations.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
+# Сложение матриц
 
+n, m = [int(i) for i in input().split()]
+matrA = [[None] * m] * n
+matrB = [[None] * m] * n
+
+for i in range(n):
+    matrA[i] = [int(j) for j in input().split()]
+input()
+for i in range(n):
+    matrB[i] = [int(j) for j in input().split()]
+
+matrC = [[0] * m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        matrC[i][j] = matrA[i][j] + matrB[i][j]
+
+for row in matrC:
+    print(*row)
