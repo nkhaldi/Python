fe2b76d3c0011e7e67039cd429756e5677ce78c1
nkhaldi <mnk80898@gmail.com>
2022-04-27 19:24:19 +0300

    modified bot

---
+++ telegram-bot/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Head or Tail Telegram bot
+# @Head_or_Tail_Bot
+# http://t.me/Head_or_Tail_Bot
+
+
+import telebot
+from random import randrange
+from telebot.types import ReplyKeyboardMarkup as keyboard
+
+
+token_file = open('/Users/narek/.pass/.head_or_tail.token')
+token = token_file.read().rstrip('\n')
+bot = telebot.TeleBot(token)
+
+board = keyboard(True, True)
+board.row('Toss')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    greeting = f"<b>Hello, {message.from_user.first_name}!</b>\nLet's play!"
+    bot.send_message(
+        message.chat.id,
+        greeting,
+        parse_mode='html',
+        reply_markup=board
+    )
+
+
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    input_message = message.text.strip().lower()
+    if input_message == 'toss':
+        rand = randrange(2)
+        msg = "Head" if rand else "Tails"
+
+        bot.send_message(
+            message.chat.id,
+            msg,
+            parse_mode='html',
+            reply_markup=board
+        )
+
+bot.polling(none_stop=True)
