a87729f7759449903360956febe137d97b381bba
nkhaldi <mnk80898@gmail.com>
2022-11-25 11:52:46 +0400

    started status backend task

---
+++ status_backend_test/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+class TreeStore:
+    pass
+
+
+items = [
+    {"id": 1, "parent": "root"},
+    {"id": 2, "parent": 1, "type": "test"},
+    {"id": 3, "parent": 1, "type": "test"},
+    {"id": 4, "parent": 2, "type": "test"},
+    {"id": 5, "parent": 2, "type": "test"},
+    {"id": 6, "parent": 2, "type": "test"},
+    {"id": 7, "parent": 4, "type": None},
+    {"id": 8, "parent": 4, "type": None}
+]
+
+ts = TreeStore(items)
