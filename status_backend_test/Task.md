6f7b0b1e6782bc1c4d79eee0a1c4dac05b0e1149
nkhaldi <mnk80898@gmail.com>
2022-11-25 13:29:15 +0400

    finifhed task

--- status_backend_test/Task.md
+++ status_backend_test/Task.md
@@ -16,23 +16,23 @@
 ## Примеры использования:
     ts.getAll()
     [
-        {"id":1,"parent":"root"},
-        {"id":2,"parent":1,"type":"test"},
-        {"id":3,"parent":1,"type":"test"},
-        {"id":4,"parent":2,"type":"test"},
-        {"id":5,"parent":2,"type":"test"},
-        {"id":6,"parent":2,"type":"test"},
-        {"id":7,"parent":4,"type":None},
-        {"id":8,"parent":4,"type":None}
+        {'id':1,'parent':'root'},
+        {'id':2,'parent':1,'type':'test'},
+        {'id':3,'parent':1,'type':'test'},
+        {'id':4,'parent':2,'type':'test'},
+        {'id':5,'parent':2,'type':'test'},
+        {'id':6,'parent':2,'type':'test'},
+        {'id':7,'parent':4,'type':None},
+        {'id':8,'parent':4,'type':None}
     ]
 ##
     ts.getItem(7)
-    {"id":7,"parent":4,"type":None}
+    {'id':7,'parent':4,'type':None}
 ##
     ts.getChildren(4)
     [
-        {"id":7,"parent":4,"type":None},
-        {"id":8,"parent":4,"type":None}
+        {'id':7,'parent':4,'type':None},
+        {'id':8,'parent':4,'type':None}
     ]
 ##
     ts.getChildren(5)
@@ -40,8 +40,8 @@
 ##
     ts.getAllParents(7)
     [
-        {"id":4,"parent":2,"type":"test"},
-        {"id":2,"parent":1,"type":"test"},
-        {"id":1,"parent":"root"}
+        {'id':4,'parent':2,'type':'test'},
+        {'id':2,'parent':1,'type':'test'},
+        {'id':1,'parent':'root'}
     ]
 ##
--- status_backend_test/main.py
+++ status_backend_test/main.py
@@ -1,18 +1,76 @@
 #!/usr/bin/env python3
 
+import json
+
+
 class TreeStore:
-    pass
+    def __init__(self, items):
+        self.tree = dict()
+        for item in items:
+            id = item['id']
+            self.tree[id] = {
+                'parent': item['parent'],
+                'children': list()
+            }
+            if 'type' in item.keys():
+                self.tree[id]['type'] = item['type']
+
+        self.items = list({'id': 'root'})
+        for item in items:
+            self.items.append(item)
+            id = item['id']
+            pid = item['parent']
+            if pid != 'root':
+                self.tree[pid]['children'].append(item)
+
+    def __str__(self):
+        return json.dumps(self.tree)
+
+    def getAll(self):
+        return self.items
+
+    def getItem(self, id):
+        return self.items[id]
+
+    def getChildren(self, id):
+        return self.tree[id]['children']
+
+    def getAllParents(self, id):
+        pid = self.tree[id]['parent']
+        parents = list()
+        while pid != 'root':
+            parent = self.items[pid]
+            parents.append(parent)
+            pid = self.tree[pid]['parent']
+        return parents
 
 
 items = [
-    {"id": 1, "parent": "root"},
-    {"id": 2, "parent": 1, "type": "test"},
-    {"id": 3, "parent": 1, "type": "test"},
-    {"id": 4, "parent": 2, "type": "test"},
-    {"id": 5, "parent": 2, "type": "test"},
-    {"id": 6, "parent": 2, "type": "test"},
-    {"id": 7, "parent": 4, "type": None},
-    {"id": 8, "parent": 4, "type": None}
+    {'id': 1, 'parent': 'root'},
+    {'id': 2, 'parent': 1, 'type': 'test'},
+    {'id': 3, 'parent': 1, 'type': 'test'},
+    {'id': 4, 'parent': 2, 'type': 'test'},
+    {'id': 5, 'parent': 2, 'type': 'test'},
+    {'id': 6, 'parent': 2, 'type': 'test'},
+    {'id': 7, 'parent': 4, 'type': None},
+    {'id': 8, 'parent': 4, 'type': None}
 ]
 
 ts = TreeStore(items)
+print('ts.getAll()')
+print(ts.getAll())
+print()
+print('ts.getItem(7)')
+print(ts.getItem(7))
+print()
+print('ts.getChildren(4)')
+print(ts.getChildren(4))
+print()
+print('ts.getChildren(5)')
+print(ts.getChildren(5))
+print()
+print('ts.getAllParents(7)')
+print(ts.getAllParents(7))
+print()
+print('ts')
+print(ts)
