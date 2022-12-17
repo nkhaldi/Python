7d8ae9e90faa494e15ad39c2450758c997d77b8b
nkhaldi <mnk80898@gmail.com>
2022-12-18 01:53:34 +0400

    moved solutions into functions

--- superannotate_task/task1.py
+++ superannotate_task/task1.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python3
 
+
+def get_numbers_from_line(line):
+    current, minus = 0, 1
+    numbers = list()
+    digits_set = '0123456789'
+
+    for char in line:
+        if char == '-':
+            minus = -1
+        elif char in digits_set:
+            current = current * 10 + int(char)
+        elif current > 0:
+            numbers.append(current * minus)
+            current, minus = 0, 1
+
+    if current > 0:
+        numbers.append(current * minus)
+
+    return numbers
+
+
+def get_sums(numbers):
+    tot_sum, pos_sum, neg_sum = 0, 0, 0
+    for number in numbers:
+        tot_sum += number
+        if number > 0:
+            pos_sum += number
+        else:
+            neg_sum += number
+
+    return tot_sum, pos_sum, neg_sum
+
+
 input_line = input("Enter line to parse: ")
 # input_line = '-100#^sdfkj8902w3ir021@swf-20'
-
-current, minus = 0, 1
-numbers = list()
-digits_set = '0123456789'
-
-for char in input_line:
-    if char == '-':
-        minus = -1
-    elif char in digits_set:
-        current = current * 10 + int(char)
-    elif current > 0:
-        numbers.append(current * minus)
-        current, minus = 0, 1
-
-if current > 0:
-    numbers.append(current * minus)
-
-pos_sum, neg_sum, tot_sum = 0, 0, 0
-for number in numbers:
-    tot_sum += number
-    if number > 0:
-        pos_sum += number
-    else:
-        neg_sum += number
+numbers = get_numbers_from_line(input_line)
+tot_sum, pos_sum, neg_sum = get_sums(numbers)
 
 print("List of found numbers:", numbers)
 print("Sum of all numbers:", tot_sum)
--- superannotate_task/task2.py
+++ superannotate_task/task2.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
+
+def caesar_encpypt(line, shift):
+    encrypted = str()
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lower = "abcdefghijklmnopqrstuvwxyz"
+
+    for char in input_line:
+        if char in upper:
+            encrypted += upper[(upper.index(char) + shift) % len(upper)]
+        elif char in lower:
+            encrypted += lower[(lower.index(char) + shift) % len(lower)]
+        else:
+            encrypted += char
+
+    return encrypted
+
+
 input_line = input("Enter line to encrypt: ")
 # input_line = 'the lazy dog jumped over the quick brown fox'
-
-
 shift = 2
-encrypted = str()
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lower = "abcdefghijklmnopqrstuvwxyz"
 
-for char in input_line:
-    if char in upper:
-        encrypted += upper[(upper.index(char) + shift) % len(upper)]
-    elif char in lower:
-        encrypted += lower[(lower.index(char) + shift) % len(lower)]
-    else:
-        encrypted += char
-
+encrypted = caesar_encpypt(input_line, shift)
 print("Encrypted message:", encrypted)
