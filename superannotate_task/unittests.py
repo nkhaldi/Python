b10c3023db59bb6aadab484156cdd22f1efc8c88
nkhaldi <mnk80898@gmail.com>
2022-12-19 00:51:37 +0400

    added simple unit-tests

---
+++ superannotate_task/unittests.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import unittest
+
+import task1 as t1
+import task2 as t2
+import task3 as t3
+
+
+class SuperAnnotateTests(unittest.TestCase):
+    def test_task1(self):
+        tests = {
+            'qwe': 0,
+            'q1w2e3': 6,
+            '11-22+33':  22,
+            '100-100': 0,
+            'qwe0qwe': 0,
+            '-100#^sdfkj8902w3ir021@swf-20': 8806,
+        }
+        for key in tests.keys():
+            print(f"{key} = {tests[key]}")
+            self.assertEqual(t1.get_numbers_sum(key), tests[key])
+
+    def test_task2(self):
+        tests = {
+            'the lazy dog jumped over the quick brown fox': 'vjg ncba fqi lworgf qxgt vjg swkem dtqyp hqz',
+            'Narek': 'Pctgm'
+        }
+        for key in tests.keys():
+            self.assertEqual(t2.caesar_encpypt(key, 2), tests[key])
+
+    def test_task3(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['C', 'D'],
+            'C': ['D'],
+            'D': ['C'],
+            'E': ['F'],
+            'F': ['C']
+        }
+        self.assertEqual(t3.find_shortest_path(graph, 'A', 'D'), ['A', 'B', 'D'])
+
+
+if __name__ == '__main__':
+    unittest.main()
