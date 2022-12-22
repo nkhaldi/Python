35753ccf81aa3e9acbf1215825a2495356c4da63
nkhaldi <mnk80898@gmail.com>
2022-12-22 21:12:36 +0400

    added source code

---
+++ tgbot_weather/config.py
@@ -0,0 +1,8 @@
+BOT_API_TOKEN = ''
+WEATHER_API_KEY = ''
+
+CURRENT_WEATHER_API_CALL = (
+        'https://api.openweathermap.org/data/2.5/weather?'
+        'lat={latitude}&lon={longitude}&'
+        'appid=' + WEATHER_API_KEY + '&units=metric'
+)
