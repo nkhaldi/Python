40127f73454b35e66cca91c91523f6f6d22a3a73
nkhaldi <mnk80898@gmail.com>
2022-12-24 23:53:31 +0400

    added modiles for states and keyboard

---
+++ tgbot_weather/inline.py
@@ -0,0 +1,12 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+BTN_TEMPERATURE = InlineKeyboardButton('Temperature', callback_data='temperature')
+BTN_WIND = InlineKeyboardButton('Wind', callback_data='wind')
+BTN_SUNTIME = InlineKeyboardButton('Suntime', callback_data='suntime')
+
+WEATHER = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND, BTN_SUNTIME)
+TEMPERATURE = InlineKeyboardMarkup().add(BTN_WIND, BTN_SUNTIME)
+WIND = InlineKeyboardMarkup().add(BTN_TEMPERATURE).add(BTN_SUNTIME)
+SUNTIME = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND)
+# HELP = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND).add(BTN_SUNTIME)
