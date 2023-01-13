9012082288f383836da51e39d82d597d4c377dd5
nkhaldi <mnk80898@gmail.com>
2023-01-13 19:54:58 +0400

    added help keyboard

--- tgbot_weather/user.py
+++ tgbot_weather/user.py
@@ -1,11 +1,18 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
 
+import inline
+
 
 async def user_start(message: Message):
     greeting = f"<b>Привет, {message.from_user.first_name}!</b>\n"
     greeting += "Укажи город"
-    await message.reply(greeting)
+    await message.reply(greeting, reply_markup=inline.HELP)
+
+
+async def user_help(message: Message):
+    help = 'Help!'
+    await message.reply(help, reply_markup=inline.HELP)
 
 
 def register_user(dp: Dispatcher):
