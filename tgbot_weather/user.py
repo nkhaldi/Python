60fef02077df5fcfdb1c2f7a445b3fa471fd40ee
nkhaldi <mnk80898@gmail.com>
2023-01-15 18:00:13 +0400

    modified help message;

--- tgbot_weather/user.py
+++ tgbot_weather/user.py
@@ -2,17 +2,23 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 
 import inline
+from states import Location
 
 
 async def user_start(message: Message):
     greeting = f"<b>Привет, {message.from_user.first_name}!</b>\n"
-    greeting += "Укажи город"
-    await message.reply(greeting, reply_markup=inline.HELP)
+    greeting += "Укажите локацию"
+    await message.reply(greeting)
+    await Location.location.set()
 
 
 async def user_help(message: Message):
-    help = 'Help!'
-    await message.reply(help, reply_markup=inline.HELP)
+    help = "Укажите локацию после комманды /set_location\n"
+    help += "И выберите нужную метрику:\n"
+    help += "/temperature - температура,\n"
+    help += "/wind - скорость и направление ветра,\n"
+    help += "/suntime - время восхода и заката солнца."
+    await message.reply(help, reply_markup=inline.WEATHER)
 
 
 def register_user(dp: Dispatcher):
