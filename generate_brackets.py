e4cf689ced1f262acc06e88cf006dfbfc56ca306
nkhaldi <mnk80898@gmail.com>
2023-01-29 22:27:21 +0400

    added new task

---
+++ generate_brackets.py
@@ -0,0 +1,19 @@
+def generate(n):
+    result = []
+
+    def generate_(left, right, stack, accum):
+        if not left and not right:
+            result.append(accum)
+            print(accum, '\n')
+            return
+        if left > 0:
+            generate_(left - 1, right, stack + 1, accum + '(')
+        if right > 0 and stack > 0:
+            generate_(left, right - 1, stack - 1, accum + ')')
+
+    generate_(n, n, 0, '')
+    return result
+
+
+if __name__ == '__main__':
+    print(generate(3))
