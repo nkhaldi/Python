95fddd162fe721b06239c010d23b980dba7fd017
nkhaldi <mnk80898@gmail.com>
2023-01-30 22:05:43 +0400

    added new task

---
+++ longest_increase.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Дан массив из  N целых чисел arr.
+Найдите длину максимальной возрастающей подпоследовательности в этом массиве.
+"""
+
+
+def longest_increase(arr):
+    dp = [1] * len(arr)
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[j] < arr[i]:
+                print(i, j, dp[i], dp[j], arr[i], arr[j])
+                dp[i] = max(dp[i], dp[j] + 1)
+                print(dp)
+    return max(dp)
+
+
+if __name__ == '__main__':
+    arr = [1, 3, 5, 4, 6, 3, 2, 7, 4]
+    print(longest_increase(arr))
