74713310325d6caf4f5ed8f7526a0b71c0cc6629
nkhaldi <mnk80898@gmail.com>
2023-02-24 01:38:36 +0400

    added script to show float numbers representations

---
+++ float_representation.py
@@ -0,0 +1,46 @@
+import struct
+from itertools import islice
+
+
+def float_to_bin_parts(number, bits=64):
+    if bits == 32:          # single precision
+        int_pack = 'I'
+        float_pack = 'f'
+        exponent_bits = 8
+        mantissa_bits = 23
+        exponent_bias = 127
+    elif bits == 64:        # double precision. all python floats are this
+        int_pack = 'Q'
+        float_pack = 'd'
+        exponent_bits = 11
+        mantissa_bits = 52
+        exponent_bias = 1023
+    else:
+        raise ValueError('bits argument must be 32 or 64')
+
+    bin_iter = iter(bin(struct.unpack(int_pack, struct.pack(float_pack, number))[0])[2:].rjust(bits, '0'))
+    return [''.join(islice(bin_iter, x)) for x in (1, exponent_bits, mantissa_bits)]
+
+
+if __name__ == '__main__':
+    initial_number = float(input("Укажите ваше число: "))
+    print("Элементы числа %.51f типа float:" % initial_number)
+    print(float_to_bin_parts(initial_number, 64))
+    sign, exponent, mantissa = float_to_bin_parts(initial_number, 64)
+
+    print("Исходный порядок в десятичном виде: " + str(int(exponent, 2)))
+    print("Настоящий порядок в двоичном виде: " + str(bin(int(exponent, 2) - 1023)))
+    print("Настоящая мантисса в двоичном виде: " + str("1." + mantissa))
+
+    exponent = int(exponent, 2)-1023-52
+    print("Порядок после переноса запятой (в десятичной системе): " + str(exponent))
+    mantissa = int("1"+mantissa, 2)
+    print("Мантисса после переноса запятой (в десятичной системе): " + str(mantissa))
+
+    recon_number = mantissa*(2.0**exponent)
+
+    if sign == '1':
+        recon_number *= -1
+
+
+    print("Реконструированное число {0:.52}".format(recon_number))
